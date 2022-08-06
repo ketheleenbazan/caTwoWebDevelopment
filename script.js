dd8d3416d43f1d68345726e3c6f5032b42121260
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2022-08-06 19:52:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,11 @@
-var textOverImages = document.getElementsByClassName("onClickTextOverImage");
-var previousTextOverImage;
-for (var i = 0; i < textOverImages.length; i++) {
-  textOverImages[i].onclick = function() {
-    var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
-    } else {
-      if (previousTextOverImage != null)
-        previousTextOverImage.classList.remove("show");
-      previousTextOverImage = this;
-      classes.add("show");
+function showText(obj)
+    {
+    var inner = obj.parentNode.getElementsByTagName("div")[0];
+    if (inner.style.display == "none")
+        inner.style.display = "";
+    else
+        inner.style.display = "none";
     }
-  }
-}
-
-function stopPropagation(event){
-  event.stopPropagation();
-}
-
-var textOverImages2 = document.getElementsByClassName("onClickTextOverImage2");
-var previousTextOverImage2;
-for (var i = 0; i < textOverImages2.length; i++) {
-  textOverImages2[i].onclick = function() {
-    var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
-    } else {
-      if (previousTextOverImage2 != null)
-        previousTextOverImage2.classList.remove("show");
-      previousTextOverImage2 = this;
-      classes.add("show");
-    }
-  }
-}
-
-function stopPropagation(event){
-  event.stopPropagation();
-}
-
-
-var textOverImages3 = document.getElementsByClassName("onClickTextOverImage3");
-var previousTextOverImage3;
-for (var i = 0; i < textOverImages3.length; i++) {
-  textOverImages3[i].onclick = function() {
-    var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
-    } else {
-      if (previousTextOverImage3 != null)
-        previousTextOverImage3.classList.remove("show");
-      previousTextOverImage3 = this;
-      classes.add("show");
-    }
-  }
-}
-
-function stopPropagation(event){
-  event.stopPropagation();
-}
-
 
 //index page DOM manipulation logo button
 function off() {
@@ -90,6 +37,76 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+// ABOUT page
+
+//This function checks if the "animate" CSS has ran, if so, it is reset so it can run again.
+
+    function jump(){
+    if (man.classList == "animate") {return}
+          man.classList.add("animate");
+          setTimeout(function(){
+        man.classList.remove("animate");
+        },250);
+      }
+
+    var endGame = setInterval(function() {
+    let manPos = parseInt(window.getComputedStyle(man).getPropertyValue("top"));
+    let blockPos = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockPos<15 && blockPos>-10 && manPos>=100){
+      block.style.animation = "none";
+      block.style.display = "none";
+      man.style.display = "none";
+      button1.style.display = "none";
+      document.getElementById("over").innerHTML = "Game over!";
+      over.style.display = "block";
+      //document.write("Game Over!");
+    }
+}, 1);
+//END
+
+// DOM manipulation
+//change images
+	function getHand1() {
+		document.getElementById('hands').src="about-images/hands2.jpg";
+	}
+
+  function getHand2() {
+		document.getElementById('hands').src="about-images/hands1.jpg";
+	}
+//END ABOUT PAGE
+
+//CONTACT PAGE
+//DOM manipulation
+//change html font colors
+function changeHeading() {
+		document.getElementById("text").innerHTML="Helping one person might not change the world but it could change the world for one person. Thank you for your help!";
+	}
+  //end
+
+// FORM Validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+//end
+//END CONTACT PAGE
 
 
 /* SIGN UP - REGISTER PAGE */
