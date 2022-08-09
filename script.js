fa110025e965ba0c6ebaef35cc46f10566f1ce1b
Ketheleen Bazan <75014452+ketheleenbazan@users.noreply.github.com>
2022-08-09 21:04:09 +0100

    last commit changes by Joseph and Ketheleen
    index / js / register updates

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//index page
+//gallery of IMAGES
+//show text when click on the image
 function showText(obj)
     {
     var inner = obj.parentNode.getElementsByTagName("div")[0];
@@ -39,33 +42,6 @@ function topFunction() {
 
 
 // ABOUT page
-
-//This function checks if the "animate" CSS has ran, if so, it is reset so it can run again.
-
-    function jump(){
-    if (man.classList == "animate") {return}
-          man.classList.add("animate");
-          setTimeout(function(){
-        man.classList.remove("animate");
-        },250);
-      }
-
-    var endGame = setInterval(function() {
-    let manPos = parseInt(window.getComputedStyle(man).getPropertyValue("top"));
-    let blockPos = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-
-    if(blockPos<15 && blockPos>-10 && manPos>=100){
-      block.style.animation = "none";
-      block.style.display = "none";
-      man.style.display = "none";
-      button1.style.display = "none";
-      document.getElementById("over").innerHTML = "Game over!";
-      over.style.display = "block";
-      //document.write("Game Over!");
-    }
-}, 1);
-//END
-
 // DOM manipulation
 //change images
 	function getHand1() {
@@ -79,20 +55,19 @@ function topFunction() {
 
 /* CONTACT PAGE */
 //DOM manipulation
-//change html font colors
 function changeHeading() {
 		document.getElementById("text").innerHTML="Helping one person might not change the world but it could change the world for one person. Thank you for your help!";
 	}
   //end
 
 // FORM Validation
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+//GOT EXTERNAL REFERENCES
+/* java validation on the forms from bootstrap */
 (function() {
   'use strict';
   window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
+    var forms = document.getElementsByClassName('check-validation');
+    // Loop
     var validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
         if (form.checkValidity() === false) {
@@ -101,18 +76,17 @@ function changeHeading() {
         }
         form.classList.add('was-validated');
       }, false);
-    });
-  }, false);
-})();
+    });}, false);})();
 //end
 //END CONTACT PAGE
 
 
 /* SIGN UP - REGISTER PAGE */
+/* java validation on the forms from bootstrap */
 (function() {
     'use strict';
     window.addEventListener('load', function() {
-      var form = document.getElementById('needs-validation');
+      var form = document.getElementById('must-validate');
       form.addEventListener('submit', function(event) {
         if (form.checkValidity() === false) {
           event.preventDefault();
@@ -120,6 +94,5 @@ function changeHeading() {
         }
         form.classList.add('was-validated');
       }, false);
-    }, false);
-  })();
+    }, false);})();
 /* END OF SIGN UP - REGISTER PAGE */
